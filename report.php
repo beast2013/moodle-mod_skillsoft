4d5435682aa1a87320437d7c49034351af771825
Leslie Shier <lesshier@gmail.com>
2017-11-28 12:24:10 -0500

    Changed back.

--- report.php
+++ report.php
@@ -216,7 +216,7 @@ if ($user) {
 
 			for ($a = $maxattempts; $a > 0; $a--) {
 				$row = array();
-				$userdata = $DB->get_record('user',array('id'=>$skillsoftuser->userid),'id, firstname, lastname, picture, imagealt, email, firstnamephonetic, lastnamephonetic');
+				$userdata = $DB->get_record('user',array('id'=>$skillsoftuser->userid),'id, firstname, lastname, picture, imagealt, email');
 
 				$row[] = $OUTPUT->user_picture($userdata, array('courseid'=>$course->id)).' '.'<a href="'.$CFG->wwwroot.'/user/view.php?id='.$skillsoftuser->userid.'&amp;course='.$course->id.'">'.fullname($userdata).'</a>';
 
